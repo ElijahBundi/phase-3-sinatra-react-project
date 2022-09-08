d6162d456f4cfa001600cbe077156a95dc31512c
elijahbundi <bundielly@gmail.com>
2022-09-08 21:47:05 +0300

    migrations and schema created

---
+++ db/migrate/20220908183441_create_blogs.rb
@@ -0,0 +1,10 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.string :content
+      t.integer :author_id
+      t.timestamps
+    end
+  end
+end
