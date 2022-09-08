f9892467949e6e2a99e4ede4a1df65d9d1ab8721
elijahbundi <bundielly@gmail.com>
2022-09-09 00:01:53 +0300

    CRUD methods added to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,45 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-
   
+  get '/blogs' do
+    blogs = Blog.all
+    blogs.map do |blog|
+      blog.title
+    end
+    blogs.to_json
+  end 
+
+  post '/reviews' do
+    review = Review.create(
+      comment: params[:comment],
+      author_id: params[:author_id],
+      blog_id: params[:blog_id]
+    )
+    review.to_json
+  end
+
+  post '/blogs' do
+    blog = Blog.create(
+      title: params[:title],
+      content: params[:content],
+      author_id: params[:author_id]
+    )
+    blog.to_json
+  end
+
+  patch '/blog/:id' do
+    blog = Blog.find(params[:id])
+    blog.update(
+      title: params[:title]
+    )
+    blog.to_json
+  end
+
+  delete '/blog/:id' do
+    blog = Blog.find(params[:id])
+    blog.destroy
+    blog.to_json
+  end
 
 end
